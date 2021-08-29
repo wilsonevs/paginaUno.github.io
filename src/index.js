f54ccc9da17a5ec83fe2a90c8299aebbbb33e493
wilsonevs <wilsonevs@gmail.com>
2021-08-29 13:19:30 -0500

    Segunda refactorización del codigo

--- src/index.js
+++
@@ -1,7 +0,0 @@
-const app = require("./config/server");
-const rutas= require("./app/routers/router.js");
-rutas(app);
-
-app.listen(app.get('port'), () =>{
-    console.log("Servidor, en el puerto: ", app.get('port'));
-})
